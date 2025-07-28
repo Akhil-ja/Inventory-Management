9ca4a010cbecf54fce04ae41f2530e62abbbd30a
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-07-28 15:06:14 +0530

    feat(frontend): Update core setup and dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes';
+import GlobalSnackbar from './components/GlobalSnackbar';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <Router>
+      <AppRoutes />
+      <GlobalSnackbar />
+    </Router>
+  );
 }
 
 export default App;
