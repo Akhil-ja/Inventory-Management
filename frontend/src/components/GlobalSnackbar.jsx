ab2ea559a13d09cca5914fcf09fb1ce175e4db97
Akhil Jagadish <akhiljagadish124@gmail.com>
2025-07-28 18:30:02 +0530

    fix(frontend): Adjust snackbar duration

--- frontend/src/components/GlobalSnackbar.jsx
+++ frontend/src/components/GlobalSnackbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Snackbar } from '@mui/material';
-import MuiAlert from '@mui/material/Alert';
-import { hideNotification } from '../slices/notification/notificationSlice';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import { hideNotification } from "../slices/notification/notificationSlice";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -13,15 +13,15 @@ function GlobalSnackbar() {
   const { isOpen, message, type } = useSelector((state) => state.notification);
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     dispatch(hideNotification());
   };
 
   return (
-    <Snackbar open={isOpen} autoHideDuration={6000} onClose={handleClose}>
-      <Alert onClose={handleClose} severity={type} sx={{ width: '100%' }}>
+    <Snackbar open={isOpen} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={type} sx={{ width: "100%" }}>
         {message}
       </Alert>
     </Snackbar>
